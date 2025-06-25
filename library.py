bb3cbcd258949cddb6853170333dcb0c768978f3
Jaryd Domine <jdomine@smu.edu>
2025-06-25 13:43:29 -0700

    lib update - stopping criterion and plots

--- library.py
+++ library.py
@@ -1,11 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from   mpl_toolkits.mplot3d import Axes3D
 import tqdm
 import os
 
 def ξ_to_x(ξ, a, b):
     """Map the points ξ in [-1, 1] (reference element) to x in [a, b]."""
     return 0.5 * (b - a) * (ξ + 1.0) + a
+
+def preprocess_exact_sol(exact_sol): 
+    assert callable(exact_sol), "exact_sol must be a callable function or None"
+    try:
+        exact_sol(0,0)
+        return exact_sol
+    except:
+        exact_sol_xμ = lambda x, μ: exact_sol(x)  # Ensure exact_sol is callable with (x, μ)
+        return  exact_sol_xμ
 
 def gausslegendre(N):
     ξ, w = np.polynomial.legendre.leggauss(N)
@@ -28,9 +38,6 @@ def gausslobatto(N):
         w[i]     = 2.0 / (N * (N-1) * (Pn_i_val**2))
     return ξ, w
 
-# print(gausslegendre(3))
-# print(gausslobatto(3))
-
 def eval_pk(ξ, i, ξ_b):
     """Compute the i-th Lagrange basis polynomial at x"""
     terms = [(ξ - ξ_b[j])/(ξ_b[i] - ξ_b[j]) for j in range(len(ξ_b)) if j != i]
@@ -48,9 +55,6 @@ def eval_pk_deriv(ξ, i, ξ_b):
             term *= (ξ - ξ_b[k]) / (ξ_b[i] - ξ_b[k])
         result += term
     return result
-
-# print(eval_pk(np.array([-1, 0, 1]), 0, np.array([-1, 0, 1])))
-# print(eval_pk_deriv(np.array([-1, 0, 1]), 0, np.array([-1, 0, 1])))
 
 def compute_mass_matrix(σ_t, a, b, Np):
     """
@@ -71,8 +75,6 @@ def compute_mass_matrix(σ_t, a, b, Np):
             Me[m, n] = (b-a)/2 * val
     return Me
 
-# print(compute_mass_matrix(lambda x: 1.0, 0, 1, 3))
-
 def compute_deriv_matrix(a, b, Np):
     """
     Full (consistent) derivative matrix on [a,b]:
@@ -91,8 +93,6 @@ def compute_deriv_matrix(a, b, Np):
                 val  += w_q[k] * dlmdx * ln
             Ge[m, n] = val
     return Ge
-
-# print(compute_deriv_matrix(0,1,3))
 
 def assemble_mass_matrix(σ_t, Np, xs):
     """Assemble matrix M"""
@@ -115,9 +115,6 @@ def assemble_deriv_matrix(Np, xs):
             for m in range(Np):
                 G[je*Np + m, je*Np + n] = G_local[m, n]
     return G
-
-# print(assemble_mass_matrix(lambda x: 1.0, 2, np.array([0.0, 0.5, 1.0])))
-# print(assemble_deriv_matrix(2, np.array([0.0, 0.5, 1.0])))
 
 def assemble_face_matrices(Np, xs, for_TSA=False):
     """Assemble matrices M^+ and M^- for face fluxes"""
@@ -163,8 +160,6 @@ def assemble_face_matrices(Np, xs, for_TSA=False):
     
     return M_plus, M_minus
 
-# print(assemble_face_matrices(2, np.array([0.0, 0.5, 1.0])))
-
 def compute_inflow_term_plus(inflow, Np, xs):
     """Compute inflow term for the left boundary (x=0)"""
     Ne = len(xs) - 1
@@ -184,9 +179,6 @@ def compute_inflow_term_minus(inflow, Np, xs):
     for m in range(Np):
         qs[je*Np + m] = inflow(xs[-1]) * eval_pk(1.0, m, ξ_b)
     return qs
-
-# print(compute_inflow_term_plus(lambda x: 1.0, 2, np.array([0.0, 0.5, 1.0])))
-# print(compute_inflow_term_minus(lambda x: 1.0, 2, np.array([0.0, 0.5, 1.0])))
 
 def compute_source_term(source, Np, xs):
     """
@@ -210,7 +202,37 @@ def compute_source_term(source, Np, xs):
             qs[je * Np + m] = (b - a) / 2.0 * val
     return qs
 
-# print(compute_source_term(lambda x: 1.0, 2, np.array([0.0, 0.5, 1.0])))
+def error_Lp(ψ_weights_all, xs, Np, exact_sol, p=2):
+    """
+    Compute the L2 error of the DG solution against an exact solution.
+    Interally uses ψ_weights_all, the ψ_weights for each μ,
+    and uses exact_sol a function of x and μ.
+    """
+    Ne = len(xs) - 1
+    ξ_b, _   = gausslobatto(Np)    # interpolation nodes for Legendre basis funcs v_m
+    ξ_q, w_q = gausslegendre(3*Np) # quadrature nodes, ξ_q in [-1, 1]
+
+    # Preprocess inputs
+    if exact_sol is not None: exact_sol = preprocess_exact_sol(exact_sol)
+    if len(ψ_weights_all.shape) == 1: ψ_weights_all = ψ_weights_all.reshape((1,-1))
+    
+    error = 0.0
+    for μ in range(ψ_weights_all.shape[0]):
+        for je in range(Ne):
+            a, b = xs[je], xs[je+1]
+            x_q  = ξ_to_x(ξ_q, a, b)   # mapped quad nodes, x_q in [a, b]
+            
+            ψ_vals = np.zeros_like(ξ_q)
+            for n in range(Np):
+                ψ_vals += ψ_weights_all[μ,je*Np+n] * eval_pk(ξ_q, n, ξ_b)
+            
+            exact_vals = exact_sol(x_q,μ)
+            diff       = ψ_vals - exact_vals
+            if p == 'inf':
+                error  = max(error, np.max(np.abs(diff)))
+            else:
+                error += np.sum(w_q * (ψ_vals - exact_vals)**p) * (b-a)/2.0
+    return error if p=='inf' else error**(1/p)
 
 def transport_direct_solve(μ:float, σ_t, source, inflow, Np, xs):
     """
@@ -230,7 +252,6 @@ def transport_direct_solve(μ:float, σ_t, source, inflow, Np, xs):
     M  = assemble_mass_matrix(σ_t, Np, xs)
     G  = assemble_deriv_matrix(Np, xs)
     F_plus, F_minus = assemble_face_matrices(Np, xs)
-    # print(np.shape(M), np.shape(G), np.shape(F_plus), np.shape(F_minus))
 
     # Compute A and inflow depending on the sign of μ
     if μ>0:
@@ -244,9 +265,10 @@ def transport_direct_solve(μ:float, σ_t, source, inflow, Np, xs):
     qs = compute_source_term(source, Np, xs) + np.abs(μ)*qs_inflow
     ψ_weights = np.linalg.solve(A, qs)
     
-    return ψ_weights.reshape((Ne,Np))
+    return ψ_weights
 
-def transport_direct_solve_diffusive(σ_t, σ_a, ε, source, inflow, Np, Nμ, Nt, xs):
+def transport_direct_solve_diffusive(σ_t, σ_a, ε, source, inflow, Np, Nμ, Nt, xs,
+                                     max_iter=1000, exact_sol=None, tol=None, tolnorm=2):
     """
     Solve the transport eq using a fixed point iteration method.
         σ_t    : Total scattering opacity func (can be a const or a func of x)
@@ -259,12 +281,20 @@ def transport_direct_solve_diffusive(σ_t, σ_a, ε, source, inflow, Np, Nμ, Nt
         Nμ     : Number of polynomial degrees in μ direction (number of Gauss-Legendre points)
         Nt     : Number of time steps for fix-point iteration
         xs     : DG mesh points defining the domain [x0, ..., xn]
-    Returns:
-        ψ_weights : Solution vector containing the weights of the polynomial basis funcs
-        μs        : Array of μ values used in the solution
-    """
-    Ne     = len(xs) - 1
 
+        max_iter : Maximum number of iterations for convergence
+        exact_sol: Optional exact solution function for validation (can be None)
+        tol      : Tolerance for convergence. If exact_sol not specified, it uses self-convergence
+        tolnorm  : Norm to use for convergence check (1,2,... or 'inf')
+    Returns:
+        ψ_weights_all : Solution vector containing the weights of the polynomial basis funcs
+        μs            : Array of μ values used in the solution
+    """
+    # Preprocess inputs
+    Ne = len(xs) - 1
+    if tolnorm == 'inf': tolnorm = np.inf
+    if exact_sol is not None: exact_sol = preprocess_exact_sol(exact_sol)
+    
     # Define scattering opacity σ_s
     if not callable(σ_t): σ_t = lambda x: σ_t
     if not callable(σ_a): σ_a = lambda x: σ_a
@@ -276,21 +306,16 @@ def transport_direct_solve_diffusive(σ_t, σ_a, ε, source, inflow, Np, Nμ, Nt
     G   = assemble_deriv_matrix(Np, xs)
     F_plus, F_minus = assemble_face_matrices(Np, xs)
     
-    ξ_μ, w_μ = gausslegendre(Nμ)
-    μs       = ξ_μ # because μ and ξ both in [-1,1]
-    # print(np.sum(w_μ))
+    μs, w_μ = gausslegendre(Nμ)
     
     ψ_weights_all = np.zeros((Nμ, Ne*Np)) # For each μ, for each element, we store the weight vector
 
     for t in tqdm.tqdm(range(Nt)):
         ψ_weights_all_old = ψ_weights_all.copy() # Store old weights for convergence check
+
         # Compute integral from -1 to 1 of ψ by quadrature
         φ = (w_μ.reshape((-1, 1)) * ψ_weights_all).sum(axis=0) # shape (Ne*Np)
-        # φ = np.zeros(Ne*Np)
-        # for i_μ, μ in enumerate(μs):
-        #     φ += w_μ[i_μ] * ψ_weights_all[i_μ]
         Msφ = 1/2 * M_s @ φ
-        # print("t",t,"Msφ:\n",Msφ)
 
         for i_μ, μ in enumerate(μs):
             # Compute A and inflow depending on the sign of μ
@@ -300,76 +325,105 @@ def transport_direct_solve_diffusive(σ_t, σ_a, ε, source, inflow, Np, Nμ, Nt
             else:
                 A = -μ * G + μ * F_minus + M_t
                 qs_inflow = compute_inflow_term_minus(lambda x: inflow(x,μ), Np, xs)
-        
             # Compute RHS
             qs = ε*(compute_source_term(lambda x: source(x, μ), Np, xs)) + np.abs(μ)*qs_inflow
-            
-            # print(np.shape(qs))
             b  = Msφ + qs
+            # Solve
             ψ_weights_all[i_μ] = np.linalg.solve(A, b)
-        # print("Convergence check:", np.linalg.norm(ψ_weights_all - ψ_weights_all_old, ord=np.inf))
-    
-    return ψ_weights_all.reshape((Nμ,Ne,Np)), μs
-
-# print(transport_direct_solve( 1.0, lambda x: 1.0, lambda x: 1.0, lambda x: 1.0, 2, np.array([0.0, 0.5, 1.0])))
-# print(transport_direct_solve(-1.0, lambda x: 1.0, lambda x: 1.0, lambda x: 1.0, 2, np.array([0.0, 0.5, 1.0])))
-
-def error_Lp(ψ_weights, xs, Np, exact_ψ_func, p=2):
-    """Compute the L2 error of the DG solution against an exact solution."""
-    Ne = len(xs) - 1
-    ξ_b, _   = gausslobatto(Np)    # interpolation nodes for Legendre basis funcs v_m
-    ξ_q, w_q = gausslegendre(3*Np) # quadrature nodes, ξ_q in [-1, 1]
-    
-    error = 0.0
-    for je in range(Ne):
-        a, b = xs[je], xs[je+1]
-        x_q  = ξ_to_x(ξ_q, a, b)   # mapped quad nodes, x_q in [a, b]
         
-        ψ_vals = np.zeros_like(ξ_q)
-        for n in range(Np):
-            ψ_vals += ψ_weights[je,n] * eval_pk(ξ_q, n, ξ_b)
-        
-        exact_vals = exact_ψ_func(x_q)
-        diff       = ψ_vals - exact_vals
-        if p == 'inf':
-            error  = max(error, np.max(np.abs(diff)))
-        else:
-            error += np.sum(w_q * (ψ_vals - exact_vals)**p) * (b-a)/2.0
-    return error if p=='inf' else error**(1/p)
+        if tol is not None:
+            if exact_sol is not None: # If exact solution is provided, use it for convergence check
+                error = error_Lp(ψ_weights_all, xs, Np, exact_sol, p=tolnorm)
+            else: # Self-convergence check
+                error = np.linalg.norm(ψ_weights_all - ψ_weights_all_old, ord=tolnorm)
+            if error < tol:
+                print(f"Converged after {t+1} iterations with tolerance {tol}.")
+                break
+    
+    return ψ_weights_all, μs
 
-def plot_solution(ψ_weights, xs, Np, μ=None, num_plot_pts=200, exact_ψ_func=None, save_plot=False):
+def plot_solution(ψ_weights_all, xs, Np, plot_3D=False, num_plot_pts=200, μ_single=None, exact_sol=None, save_plot=False):
     """
     Reconstructs and plots the DG solution ψ(x) over the mesh xs.
-
     Plot optional:  saved to "test_figures/transport_solution.png" if save_plot=True.
     """
+    # Preprocess inputs
+    if exact_sol is not None: exact_sol = preprocess_exact_sol(exact_sol)
+    if len(ψ_weights_all.shape) == 1:
+        ψ_weights_all = ψ_weights_all.reshape((1,-1))
+        assert μ_single is not None, "For solution at single μ, μ_single must be provided."
+
     Ne     = len(xs) - 1
     ξ_b, _ = gausslobatto(Np)                 # interpolation nodes for Legendre basis ("b") funcs v_m
     ξ_p    = np.linspace(-1, 1, num_plot_pts) # plot points (in reference space [-1,1]) for each element
+    if μ_single is not None: μs = [μ_single]
+    else: μs,_ = gausslegendre(ψ_weights_all.shape[0])
 
-    plt.figure()
-    for je in range(Ne):
-        a, b = xs[je], xs[je+1]
-        x_p  = ξ_to_x(ξ_p, a, b)
-        
-        # Reconstruct polynomial on this element
-        ψ_weights_loc = ψ_weights[je,:]
-        ψ_vals = np.zeros_like(ξ_p)
-        for n in range(Np):
-            ψ_vals += ψ_weights_loc[n] * eval_pk(ξ_p, n, ξ_b)
-        plt.plot(x_p, ψ_vals, '-')
+    # Create figure
+    fig = plt.figure(figsize=(6, 5))      
+    if plot_3D:
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_xlabel('x')
+        ax.set_ylabel('μ')
+        ax.set_zlabel(r'$\psi(x,\mu)$')
+        ax.grid(False)
+        ax.set_xlim(xs[0], xs[-1])
+        ax.set_ylim(-1, 1)
+        ax.set_zlim(0, 3.65)
+        ax.set_xticks([0, 1])
+        ax.set_yticks([-1, 0, 1])
+    else:
+        ax = fig.add_subplot(111)
+        ax.set_xlabel('x')
+        ax.set_ylabel(r'$\psi(x,\mu)$')
 
-    if exact_ψ_func is not None:
-        exactx = np.linspace(xs[0], xs[-1], num_plot_pts)
-        exactψ = exact_ψ_func(exactx)
-        plt.plot(exactx, exactψ, 'k--', label='Exact solution')
-        plt.legend()
+    for iμ,μ in enumerate(μs):
+        maxψ,minψ = 0, 0
+        for je in range(Ne):
+            a, b = xs[je], xs[je+1]
+            numx = ξ_to_x(ξ_p, a, b)
+            ψ_weights_loc = ψ_weights_all[iμ,je*Np:(je+1)*Np]
+            numψ = np.zeros_like(ξ_p)
+            for n in range(Np):
+                numψ += ψ_weights_loc[n] * eval_pk(ξ_p, n, ξ_b)
+            maxψ,minψ = max(maxψ, np.max(numψ)), min(minψ, np.min(numψ))
+        if plot_3D:
+            shadowx = np.linspace(xs[0], xs[-1], num_plot_pts)
+            shadowψ = exact_sol(shadowx,μ)
+            ax.plot(shadowx, np.full_like(shadowx, μ), np.zeros_like(shadowx), '-', color='darkgray', lw=1.2)
+            ax.plot(np.full_like(shadowx, 0), np.full_like(shadowx, μ), np.linspace(minψ,maxψ,len(shadowx)), '-', color='darkgray', lw=1.2)
 
-    plt.xlabel('x')
-    plt.ylabel(r'$\psi(x)$')
-    if μ is not None:
-        plt.title(f'DG solution, μ={μ}, Np={Np}, Ne={Ne}')
-    plt.grid(True)
+    clrs = ['cornflowerblue', 'darkorange', 'forestgreen', 'crimson', 'purple']
+    for iμ,μ in reversed(list(enumerate(μs))):
+        maxψ,minψ = 0, 0
+        for je in range(Ne):
+            a, b = xs[je], xs[je+1]
+            numx  = ξ_to_x(ξ_p, a, b)
+            
+            # Reconstruct polynomial on this element
+            ψ_weights_loc = ψ_weights_all[iμ,je*Np:(je+1)*Np]
+            numψ = np.zeros_like(ξ_p)
+            for n in range(Np):
+                numψ += ψ_weights_loc[n] * eval_pk(ξ_p, n, ξ_b)
+            maxψ,minψ = max(maxψ, np.max(numψ)), min(minψ, np.min(numψ))
+            if plot_3D:
+                ax.plot(numx, np.full_like(numx, μ), numψ, '-', color=clrs[iμ%len(clrs)], lw=2.5)
+            else:
+                ax.plot(numx, numψ, '-', color=clrs[iμ%len(clrs)], lw=2.5)
+
+        if exact_sol is not None:
+            exactx = np.linspace(xs[0], xs[-1], num_plot_pts)
+            exactψ = exact_sol(exactx,μ)
+            if plot_3D:
+                ax.plot(exactx, np.full_like(exactx, μ), exactψ, '--', color='black', lw=1.25)
+                # ax.plot(exactx, np.full_like(exactx, μ), np.zeros_like(exactx), '-', color='darkgray', lw=1.2)
+            else:
+                ax.plot(exactx, exactψ, '--', color='black', lw=1.25)
+            plt.legend()
+
+    if μs is not None:
+        if len(μs) == 1: plt.title(f'DG solution, μ={μs}, Np={Np}, Ne={Ne}')
+        else: plt.title(f'DG solution, Np={Np}, Ne={Ne}')
     
     if save_plot:
         import os
@@ -382,3 +436,45 @@ def plot_solution(ψ_weights, xs, Np, μ=None, num_plot_pts=200, exact_ψ_func=N
     else:
         plt.show()
 
+# def plot_solution_3d(ψ_weights_all, xs, Np, num_plot_pts=200, μ_single=None, exact_sol=None, save_plot=False):
+#     """
+#     Reconstructs and plots the DG solution ψ(x, μ) over the mesh xs.
+#     Plot optional:  saved to "test_figures/transport_solution_3d.png" if save_plot=True.
+#     """
+#     # Preprocess inputs
+#     if exact_sol is not None: exact_sol = preprocess_exact_sol(exact_sol)
+#     if len(ψ_weights_all.shape) == 1:
+#         ψ_weights_all = ψ_weights_all.reshape((1,-1))
+#         assert μ_single is not None, "For solution at single μ, μ_single must be provided."
+
+#     Ne     = len(xs) - 1
+#     ξ_b, _ = gausslobatto(Np)                 # interpolation nodes for Legendre basis ("b") funcs v_m
+#     ξ_p    = np.linspace(-1, 1, num_plot_pts) # plot points (in reference space [-1,1]) for each element
+#     if μ_single is not None: μs = [μ_single]
+#     else: μs,_ = gausslegendre(ψ_weights_all.shape[0])
+
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
+    
+#     for iμ,μ in enumerate(μs):
+#         for je in range(Ne):
+#             a, b = xs[je], xs[je+1]
+#             x_p  = ξ_to_x(ξ_p, a, b)
+            
+#             # Reconstruct polynomial on this element
+#             ψ_weights_loc = ψ_weights_all[iμ,je*Np:(je+1)*Np]
+#             ψ_vals = np.zeros_like(ξ_p)
+#             for n in range(Np):
+#                 ψ_vals += ψ_weights_loc[n] * eval_pk(ξ_p, n, ξ_b)
+            
+#             ax.plot(x_p, np.full_like(x_p, μ), ψ_vals)
+
+#         if exact_sol is not None:
+#             exactx = np.linspace(xs[0], xs[-1], num_plot_pts)
+#             exactψ = exact_sol(exactx,μ)
+#             ax.plot(exactx, np.full_like(exactx, μ), exactψ, 'k--', label='Exact solution')
+#             ax.legend()
+
+#     ax.set_xlabel('x')
+#     ax.set_ylabel('μ')
+#     ax.set_zlabel(r'$\psi(x,\mu)$')
